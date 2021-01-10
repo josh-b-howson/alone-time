76653fcbb03fe827e1d8fb8de149ac4a24a59a29
RVT-Josh <josh@rvt.com>
2021-01-10 14:06:51 -0800

    Remove default stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+const AloneTimeApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default AloneTimeApp;
