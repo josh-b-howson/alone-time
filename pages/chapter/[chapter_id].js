bfb0537373d6bcabd752ca2c0b773a00be926816
RVT-Josh <josh@rvt.com>
2021-01-29 22:49:34 -0800

    Fix Next/previous chapter links with version param

--- pages/chapter/[chapter_id].js
+++ pages/chapter/[chapter_id].js
@@ -5,14 +5,15 @@ import { getChapter } from "../../utils/bibleConnector";
 
 const Read = (props) => {
   const chapter = props.chapter;
+  const versionIdFromQuery = props.query.version;
 
   return <Layout {...props} title={`Read ${chapter.reference}`}>
     <h1>{chapter.reference}</h1>
     <main className="read">
       <BibleContent content={chapter.content} />
     </main>
-    {chapter.previous && <Link href={`/chapter/${chapter.previous.id}`}><a>Previous Chapter</a></Link>}
-    {chapter.next && <Link href={`/chapter/${chapter.next.id}`}><a>Next Chapter</a></Link>}
+    {chapter.previous && <Link href={`/chapter/${chapter.previous.id}?version=${versionIdFromQuery}`}><a>Previous Chapter</a></Link>}
+    {chapter.next && <Link href={`/chapter/${chapter.next.id}?version=${versionIdFromQuery}`}><a>Next Chapter</a></Link>}
   </Layout>
 }
 
@@ -23,7 +24,7 @@ export async function getServerSideProps(ctx) {
     .then(json => json.data)
     .catch(res => console.error(`An error occurred in getChapter(). ${res.error}`));
 
-  return { props: { chapter: chapter } }
+  return { props: { query:query, chapter: chapter } }
 }
 
 export default Read;
