bef96ffc24e9c076b65a5302a514efc5ed2dc545
RVT-Josh <josh@rvt.com>
2021-01-30 01:28:38 -0800

    Add featured versions to VersionSelect

--- components/VersionSelect.js
+++ components/VersionSelect.js
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setVersionId } from '../store/actions/version';
 import { getAllVersions } from '../utils/bibleConnector';
-import { limitCharacters } from '../utils/utils';
+import { getFeaturedVersions, limitCharacters } from '../utils/utils';
 
 const VersionSelect = (props) => {
   const [versionList, setVersionList] = useState(null);
+  const [featuredVersionList, setFeaturedVersionList] = useState(null);
   const currentVersion = props.currentVersion;
 
   // fetch list of versions from API, add to version list state
@@ -16,8 +17,11 @@ const VersionSelect = (props) => {
       .then(json => json.data)
       .catch(res => console.error(`getAllVersions() failed. ${res}`))
 
-    if (versions.length > 0)
+    if (versions.length > 0) {
       setVersionList(versions)
+      setFeaturedVersionList(getFeaturedVersions(versions));
+
+    }
     else
       console.error("versionList wasn't able to be set. Nothing returned from fetch.")
   }
@@ -40,15 +44,26 @@ const VersionSelect = (props) => {
   return <div>
     <select onChange={handleChange}>
       <option value="none">Choose a version</option>
-      {versionList && versionList.map(version =>
-        <option
-          key={version.id}
-          value={version.id}
-          selected={currentVersion && version.id === currentVersion.id ? 'selected' : ''}
-        >
-          {limitCharacters(`${version.abbreviation} - ${version.name}`, { limit: 40, ellipsis: true })}
-        </option>
-      )}
+      {featuredVersionList &&
+        <optgroup label="Featured">
+          {featuredVersionList.map(version =>
+            <option
+              key={version.id}
+              value={version.id}>
+              {limitCharacters(`${version.name} - ${version.abbreviation}`, { limit: 40, ellipsis: true })}
+            </option>)}
+        </optgroup>}
+      {versionList &&
+        <optgroup label="All">
+          {versionList.map(version =>
+            <option
+              key={version.id}
+              value={version.id}
+              selected={currentVersion && version.id === currentVersion.id ? 'selected' : ''}>
+              {limitCharacters(`${version.name} - ${version.abbreviation}`, { limit: 40, ellipsis: true })}
+            </option>)}
+        </optgroup>
+      }
     </select>
   </div>
 }
--- utils/utils.js
+++ utils/utils.js
@@ -13,3 +13,20 @@ export const limitCharacters = (value, options) => {
   else
     return value.substring(0, options.limit);
 }
+
+export const getFeaturedVersions = (versions) => {
+  const featuredVersions = versions.map(version => {
+    const featuredItems = [
+      '06125adad2d5898a-01',
+      '65eec8e0b60e656b-01',
+      'de4e12af7f28f599-02',
+      '2f0fd81d7b85b923-01',
+      'c315fa9f71d4af3a-01',
+      '01b29f4b342acc35-01',
+    ]
+    if (featuredItems.includes(version.id))
+      return version;
+  });
+
+  return featuredVersions.filter(i => !!i);
+}
